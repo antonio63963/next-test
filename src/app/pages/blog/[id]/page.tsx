c269e2933fbbce8dd19d6bae854e1adef7567cc1
Anton <60780638+antonio63963@users.noreply.github.com>
2023-09-11 21:33:35 +0300

    getting data

--- src/app/pages/blog/[id]/page.tsx
+++ src/app/pages/blog/[id]/page.tsx
@@ -7,16 +7,30 @@ type Props = {
   }
 };
 
-export async function generateMetadata({params: {id}}: Props): Promise<Metadata> {
+export async function generateMetadata({ params: { id } }: Props): Promise<Metadata> {
+  const post = await getData(id);
   return {
-    title: id,
+    title: post.title,
   };
 }
 
-export default function Post({params: {id}}: Props) {
+async function getData(id: string) {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+    next: {
+      revalidate: 60
+    }
+  });
+  return response.json();
+}
+
+export default async function Post({ params: { id } }: Props) {
+  const post = await getData(id);
+  console.log(post);
+
   return (
     <div>
-      <h1>Post page {id}</h1>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
     </div>
   );
 }
--- src/app/pages/blog/page.tsx
+++ src/app/pages/blog/page.tsx
@@ -1,14 +1,34 @@
 import { Metadata } from 'next';
+import Link from 'next/link';
 import React from 'react';
 
 export const metadata: Metadata = {
   title: 'Blog | Next App',
 };
 
-export default function Blog() {
+async function getData() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+    next: {
+      revalidate: 60
+    }
+  });
+  return response.json();
+}
+
+export default async function Blog() {
+  const posts = await getData();
+
   return (
     <div>
-      <h1>Blog</h1>
+      <h1>Blog page</h1>
+      <ul>
+        
+      {posts.map((post: any) => {
+        return <li key={post.id}>
+          <Link href={`/pages/blog/${post.id}`}>{post.title}</Link>
+        </li>; 
+      })}
+      </ul>
     </div>
   );
 }
